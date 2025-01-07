1ff62a0d64dba54b73b4fa206b7035615e19ebd8
sakshamengineer <118796731+sakshamengineer@users.noreply.github.com>
2025-01-07 20:00:55 +0530

    Add files via upload

---
+++ C1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+    int a,b,c;
+    printf("enter the marks of 1st subject:\t");
+    scanf("%d",&a);
+    printf("enter the marks of 2nd subject:\t");
+    scanf("%d",&b);
+    printf("enter the marks of 3rd subject:\t");
+    scanf("%d",&c);
+    int d =(a+b+c)/3;
+    if (a<33 || b<33 || c<33)
+    {
+        printf("You are failed due to low score in particular subject");
+    }
+    else{
+        if (d<40)
+        {
+            printf("You are failed due to overall low percentage");
+        }
+        else{
+            printf("You are passed!");
+        }
+    } 
+    return 0;
+}
